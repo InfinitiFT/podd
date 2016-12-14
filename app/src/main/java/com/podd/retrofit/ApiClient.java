f7096a223c162bc260f70fcee5654d2353f00bc3
kartiki.tripathi@mobiloitte.in <kartiki.tripathi@mobiloitte.in>
2016-12-14 19:50:51 +0530

    take pull

---
+++ app/src/main/java/com/podd/retrofit/ApiClient.java
@@ -0,0 +1,72 @@
+package com.podd.retrofit;
+
+
+import android.content.Context;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class ApiClient {
+
+    public static final String BASE_URL = "https://247mytutor.mobiloitte.com:8080/";
+
+    private static Retrofit retrofit = null;
+
+
+
+    public static Retrofit getClient(Context context) {
+        if (retrofit==null) {
+
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request request = original.newBuilder()
+                            .header("Content-Type", "application/json")
+                            .method(original.method(), original.body())
+                            .build();
+
+                    return chain.proceed(request);
+                }
+            });
+
+
+            HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+            httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = null;
+            try {
+                client = new OkHttpClient.Builder()
+                        .addInterceptor(httpLoggingInterceptor)
+                        .readTimeout(30, TimeUnit.SECONDS)
+                        .connectTimeout(30, TimeUnit.SECONDS)
+
+                        .build();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+
+                    .build();
+
+        }
+        return retrofit;
+    }
+
+
+}
---
+++ app/src/main/java/com/podd/retrofit/ApiInterface.java
@@ -0,0 +1,179 @@
+package com.podd.retrofit;
+
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+
+public interface ApiInterface {
+
+    /*@POST("signup")
+    Call<JsonResponse> getSignUp(@Body JsonRequest loginRequestModel);
+
+    @POST("changePassword")
+    Call<JsonResponse> getChangePassword(@Header("token") String token, @Body JsonRequest changePasswordRequestModel);
+
+    @POST("login")
+    Call<JsonResponse> getLogin(@Body JsonRequest loginModel);
+
+    @POST("tutorpage")
+    Call<JsonResponse> getSearchTutor(@Header("token") String token, @Body JsonRequestTutor searchTutorModel);
+
+    @POST("mentorpage")
+    Call<JsonResponse> getSearchMentor(@Header("token") String token, @Body JsonRequestTutor searchTutorModel);
+
+    @POST("forgotPass")
+    Call<JsonResponse> getforgotPass(@Header("token") String token, @Body JsonRequest forgotPassword);
+
+
+    @POST("setAppointTime")
+    Call<JsonResponse> setAppointTime(@Header("token") String token, @Body JsonRequest setAppointmentTime);
+
+
+    @POST("TakeAppointment")
+    Call<JsonResponse> setTakeAppointment(@Header("token") String token, @Body JsonRequest takeAppointment);
+
+    @POST("AvailableTiming")
+    Call<JsonResponse> getAvailableTime(@Header("token") String token, @Body JsonRequest availableTime);
+
+    @POST("changeNotificationStatus")
+    Call<JsonResponse> getNotificationStatus(@Header("token") String token, @Body JsonRequest notificationStatus);
+
+    @POST("calculatePoint")
+    Call<JsonResponse> calculatePoints(@Header("token") String token, @Body JsonRequest calculatePoints);
+
+    @POST("bookinghistory")
+    Call<BookingHistoryModel> getBookingHistory(@Header("token") String token, @Body JsonRequest bookingHistory);
+
+    @POST("paynow")
+    Call<JsonResponse> getPayNow(@Header("token") String token, @Body JsonRequest payNow);
+
+    @POST("editrating")
+    Call<JsonResponse> getRating(@Header("token") String token, @Body JsonRequest payNow);
+
+    @POST("comments")
+    Call<JsonResponseComment> getComment(@Header("token") String token, @Body JsonRequest comment);
+
+    @POST("tutorpagebuy")
+    Call<LessonModal> getLessonBuy(@Header("token") String token, @Body JsonRequest lessonBuy);
+
+    @POST("lessonBuy")
+    Call<JsonResponse> getBuyLesson(@Header("token") String token, @Body JsonRequest buyLesson);
+
+    @POST("bankdetail")
+    Call<JsonResponseBankDetail> getBankDetail(@Header("token") String token, @Body JsonRequest bankDetail);
+
+    @POST("paypalDetail")
+    Call<JsonResponsePaypal> getPaypalDetail(@Header("token") String token, @Body JsonRequest paypal);
+    @POST("addlessonschedule")
+    Call<JsonResponse> getAddLessonSchedule(@Header("token") String token, @Body JsonRequest addLesson);
+
+    @POST("mentorhome")
+    Call<HomeModel> getMentorHome(@Header("token") String token, @Body JsonRequest homeMentor);
+
+    @POST("studenthome")
+    Call<HomeModel> getStudentHome(@Header("token") String token, @Body JsonRequest homeMentor);
+
+    @POST("allsubject")
+    Call<TakeAppoint> getAllSubject(@Header("token") String token, @Body JsonRequest allSubject);
+
+
+    @POST("PointSharing")
+    Call<JsonResponse> getRewardPoint(@Header("token") String token, @Body JsonRequest jsonRequest);
+
+    @POST("chatHistory")
+    Call<JsonResponseChatHistory> getChatHistory(@Header("token") String token, @Body JsonRequest jsonRequest);
+
+    @POST("logout")
+    Call<JsonResponse> getLogout(@Header("token") String token, @Body JsonRequest logout);
+
+    @POST("languageExchange")
+    Call<JsonResponse> getToMentor(@Header("token") String token, @Body JsonRequest imageapi);
+    @POST("imageApi")
+    Call<JsonResponseImage> getUserImage(@Header("token") String token, @Body JsonRequest imageapi);
+
+    @POST("notificationHistory")
+    Call<JsonResponseNotification> getNotification(@Header("token") String token, @Body JsonRequest notiapi);
+
+    @GET("countryname")
+    Call<CountryModal> countryName(@Header("token") String token);
+
+    @GET("countryname")
+    Call<CountryListModel> countryListName(@Header("token") String token);
+
+    @GET("profile/{id}")
+    Call<ProfileModal> calledGetProfile(@Header("token") String token, @Path("id") String id);
+
+    @GET("profile/{id}")
+    Call<TutorProfileModel> calledTutorProfile(@Header("token") String token, @Path("id") String id);
+
+    @GET("profile/{id}")
+    Call<MentorProfileModel> calledMentorProfile(@Header("token") String token, @Path("id") String id);
+
+    @GET("getPoint/{id}")
+    Call<JsonResponse> getPointApi(@Header("token") String token, @Path("id") String id);
+
+    @GET("http://api.fixer.io/latest?base=USD")
+    Call<JsonResponse> calculateCurrencyPoints(@Header("token") String token);
+
+    @GET("termsView")
+    Call<JsonResponse> getTerms();
+
+
+    @GET("tutorSubjectList")
+    Call<JsonResponseSubjectList> getSubjectList();
+
+
+
+    @PUT("edit/{id}")
+    Call<JsonResponse> calledEditProfile(@Header("token") String token, @Path("id") String id, @Body JsonRequest editProfile);
+
+    @PUT("edit/{id}")
+    Call<JsonResponse> calledEditTutor(@Header("token") String token, @Path("id") String id, @Body TutorEditProfileResult editProfile);
+
+    @PUT("edit/{id}")
+    Call<JsonResponse> calledEditMentor(@Header("token") String token, @Path("id") String id, @Body MentorEditProfileResult editProfile);
+
+    @POST("ViewStudent")
+    Call<ViewUserResult> getStudentUserList(@Header("token") String token, @Body JsonRequest viewStudentList);
+
+    @POST("appointments")
+    Call<AppointmentModel> getAppointmentsList(@Header("token") String token, @Body JsonRequest appointments);
+
+    @POST("AcceptUser")
+    Call<AcceptDeclineResponse> acceptUser(@Header("token") String token, @Body JsonRequest AcceptUser);
+
+    @POST("RejectUser")
+    Call<AcceptDeclineResponse> rejectUser(@Header("token") String token, @Body JsonRequest RejectUser);
+
+    @POST("checkemail")
+    Call<TutorEmailCheckResponse> emailCheck(@Body TutorEmailCheckRequest loginModel);
+
+    @GET("agreementView")
+    Call<ExchangeAgreementResponse> callExchangeAgreement();
+
+    @GET("showallQuestions")
+    Call<LanguageHelpResponse> callShowAllQuestions(@Header("token") String token);
+
+    @GET("studentmylesson/{id}")
+    Call<StudentMyLessonResponse> callStudentMyLesson(@Header("token") String token, @Path("id") String id);
+
+    @POST("LanguageHelp")
+    Call<ResultResponse> languageHelp(@Header("token") String token, @Body LanguageHelpRequest languageHelpRequest);
+
+    @POST("AnswerTheQuestion")
+    Call<AnswerResponse> answerTheQuestions(@Header("token") String token, @Body AllAnswerRequest allAnswerRequest);
+
+    @POST("studentlessonsearch")
+    Call<LessonSearchResponse> studentLessonSearch(@Header("token") String token, @Body StudentLessonSearchRequest studentLessonSearchRequest);
+
+    @POST("stripe")
+    Call<StripeJsonResponse> getStripePay(@Header("token") String token, @Body JsonRequest jsonRequest);*/
+
+}
